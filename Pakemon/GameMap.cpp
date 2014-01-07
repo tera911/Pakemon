56e595a33326dff4218fe3c97628c178ad99944b
manyu <B2020@oic.jp>
2014-01-07 18:22:52 +0900

    MAPのスクロールを実装

--- Pakemon/GameMap.cpp
+++ Pakemon/GameMap.cpp
@@ -1,14 +1,49 @@
 #include "GameMap.h"
 #include "Nyancat.h"
 
+void GameMap::render(){
+	for(int x = 0; x < MAP_WIDTH; x++){
+		for(int y = 0; y < MAP_HEIGHT; y++){
+			render_block(map[x][y], x, y);		
+		}
+	}
+	screenScroll_x(0.02f);
+}
+void GameMap::render_block(int block_type, int x, int y){
+	if(block_type == 0){
+		//return;
+	}
+	int block_x, block_y;
+	block_x =  x * 32 - screen_x * 32;
+	block_y  = y * 32;
+	switch(block_type){
+		case 'A':
+			DrawGraph(block_x, block_y, block_brick, true);
+		break;
+		case 'S':
+			DrawGraph(block_x, block_y, block_ware, true);
+		break;
+		case 'I':
+			DrawGraph(block_x, block_y, block_hatena, true);
+		break;
+		case 'C':
+			DrawGraph(block_x, block_y, block_packet, true);
+		break;
+		default:
+			DrawString(block_x + 16, block_y + 16, "1", GetColor(255,255,255));
+			return;
+		break;
+	}
+}
+
 int GameMap::checkMapHit(Nyancat* nyan){
 		float nyan_x = nyan->getNyanX();
 		float nyan_y = nyan->getNyanY();
 		int tb[2], bb[2], cb[2],lb[2], rb[2];		//上、下、中央、左、右ブロック [x,y] 
 
 		//ブロックにめり込むのを防ぐ
-		cb[0] = round(nyan_x / 32.0f);	//自キャラのX座標
-		cb[1] = round(nyan_y / 32.0f);	//自キャラのY座標
+		cb[0] = round((nyan_x + screen_x * 32) / 32.0f);	//自キャラのX座標
+		cb[1] = round((nyan_y) / 32.0f);	//自キャラのY座標
 
 		tb[0] = cb[0];		//自キャラの上ブロックのx座標
 		tb[1] = cb[1] -1;	//自キャラの上ブロックのy座標
@@ -45,15 +80,15 @@ int GameMap::checkMapHit(Nyancat* nyan){
 			}
 		}
 		//左に飛び出た
-		if(cb[0] * 32 > nyan_x){
+		if(cb[0] * 32 > (nyan_x + screen_x * 32)){
 			if(map[lb[0]][lb[1]] == 'A' || map[lb[0]][lb[1]] == 'S' || map[lb[0]][lb[1]] == 'I'){
-				nyan->revisePosition(1, cb[0] * 32 - nyan_x);	//位置修正
+				nyan->revisePosition(1, cb[0] * 32 - (nyan_x + screen_x * 32));	//位置修正
 			}
 		}
 		//右に飛び出た
-		if(cb[0] * 32 < nyan_x){
+		if(cb[0] * 32 < (nyan_x + screen_x * 32)){
 			if(map[rb[0]][rb[1]] == 'A' || map[rb[0]][rb[1]] == 'S' || map[rb[0]][rb[1]] == 'I'){
-				nyan->revisePosition(3, nyan_x - cb[0] * 32);	//位置修正
+				nyan->revisePosition(3, (nyan_x + screen_x * 32) - cb[0] * 32);	//位置修正
 			}
 		}
 		//自キャラがブロックにめり込んだ場合　
@@ -66,10 +101,10 @@ int GameMap::checkMapHit(Nyancat* nyan){
 				nyan->revisePosition(0, block_pos[1] - nyan_y);
 			}else if(block_pos[1] < nyan_y){
 				nyan->revisePosition(2, nyan_y - block_pos[1]);
-			}else if(block_pos[0] > nyan_x){
-				nyan->revisePosition(3, block_pos[0] - nyan_x);
-			}else if(block_pos[0] < nyan_x){
-				nyan->revisePosition(1, nyan_x - block_pos[0]);
+			}else if(block_pos[0] > (nyan_x + screen_x * 32)){
+				nyan->revisePosition(3, block_pos[0] - (nyan_x + screen_x * 32));
+			}else if(block_pos[0] < (nyan_x + screen_x * 32)){
+				nyan->revisePosition(1, (nyan_x + screen_x * 32) - block_pos[0]);
 			}
 		}
 
@@ -79,37 +114,47 @@ int GameMap::checkMapHit(Nyancat* nyan){
 		}
 		
 
-		int block_x = (nyan_x / 32) + 1;
+		int block_x = ((nyan_x + screen_x * 32) / 32) + 1;
 		int block_y = (nyan_y / 32) + 1;
-		DrawBox(cb[0] * 32, cb[1] * 32, (cb[0] * 32) + 32,(cb[1] * 32) +  32, GetColor(255,0,0), true);
+
+		DrawBox(cb[0] * 32 - screen_x * 32,
+			cb[1] * 32, 
+			(cb[0] * 32 - screen_x * 32) + 32, 
+			(cb[1] * 32) +  32, 
+			GetColor(255,0,0), 
+			true);
+
 		if(map[cb[0]][cb[1]] != 0){
 			DrawString(500, 100, "接触", GetColor(255,255,255));
 		}
 		return 0;
 	}
 int GameMap::checkMapHit1(float nyan_x, float nyan_y, int direction, float value){
-		int tb[2], bb[2], cb[2],lb[2], rb[2];		//上、下、中央、左、右ブロック [x,y] 
-		cb[0] = round(nyan_x / 32.0f);	//自キャラのX座標
-		cb[1] = round(nyan_y / 32.0f);	//自キャラのY座標
+	int tb[2], bb[2], cb[2],lb[2], rb[2];		//上、下、中央、左、右ブロック [x,y] 
+	cb[0] = round(nyan_x / 32.0f);	//自キャラのX座標
+	cb[1] = round(nyan_y / 32.0f);	//自キャラのY座標
 		
-		tb[0] = cb[0];		//自キャラの上ブロックの座標
-		tb[1] = cb[1] -1;	//自キャラの上ブロックの座標
+	tb[0] = cb[0];		//自キャラの上ブロックの座標
+	tb[1] = cb[1] -1;	//自キャラの上ブロックの座標
 		
-		bb[0] = cb[0];		//自キャラの上ブロックの座標
-		bb[1] = cb[1] +1;	//自キャラの上ブロックの座標
+	bb[0] = cb[0];		//自キャラの上ブロックの座標
+	bb[1] = cb[1] +1;	//自キャラの上ブロックの座標
 
-		switch(direction){
-		case 0:
-			//上
-			if(map[tb[0]][tb[1]] == 'A' || map[tb[0]][tb[1]] == 'S' || map[tb[0]][tb[1]] == 'I'){
-					int next_value = nyan_y - cb[1] * 32;
-					if(next_value > value){
-						return next_value;
-					}else{
-						return value;
-					}
+	switch(direction){
+	case 0:
+		//上
+		if(map[tb[0]][tb[1]] == 'A' || map[tb[0]][tb[1]] == 'S' || map[tb[0]][tb[1]] == 'I'){
+				int next_value = nyan_y - cb[1] * 32;
+				if(next_value > value){
+					return next_value;
+				}else{
+					return value;
 				}
-		break;
-		}
-		return value;
+			}
+	break;
 	}
+	return value;
+}
+void GameMap::screenScroll_x(float value){
+	screen_x = screen_x + value;
+}
--- Pakemon/GameMap.h
+++ Pakemon/GameMap.h
@@ -31,45 +31,13 @@ public:
 		builder.getMap(map);
 
 	}
-	void render(){
-		int i, j;
-		for(i = 0; i < MAP_WIDTH; i++){
-			for(j = 0; j < MAP_HEIGHT; j++){
-				render_block(map[i][j], i, j);		
-			}
-		}
-	}
-	void render_block(int block_type, int x, int y){
-		if(block_type == 0){
-			//return;
-		}
-		int block_x, block_y;
-		block_x =  x * 32 - screen_x * 4;
-		block_y  = y * 32;
-		switch(block_type){
-			case 'A':
-				DrawGraph(block_x, block_y, block_brick, true);
-			break;
-			case 'S':
-				DrawGraph(block_x, block_y, block_ware, true);
-			break;
-			case 'I':
-				DrawGraph(block_x, block_y, block_hatena, true);
-			break;
-			case 'C':
-				DrawGraph(block_x, block_y, block_packet, true);
-			break;
-			default:
-				DrawString(block_x + 16, block_y + 16, "1", GetColor(255,255,255));
-				return;
-			break;
-		}
-	}
+	void render();
+	void render_block(int block_type, int x, int y);
 	//自キャラX、自キャラY、　方向、　移動させたい距離
 	int checkMapHit1(float nyan_x, float nyan_y, int direction, float value);
 
 	int checkMapHit(Nyancat* nyan);
-	void screenScroll_x(int value);
+	void screenScroll_x(float value);
 };
 
 #endif
